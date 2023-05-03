417ec2bcf7951dde1a3085e9d57e295bfce238ce
Nickson Anthony Pereira <nick3on@ahtcloud.com>
2023-05-03 22:11:31 +0530

    this is first commit

---
+++ rbca21213/src/rbca21213/orange.java
@@ -0,0 +1,16 @@
+package rbca21213;
+
+public class orange {
+String color;
+orange(String color)
+{
+this.color=color;
+}
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+orange greenApple = new orange(&quot;green&quot;);
+String color = greenApple.getColor();
+System.out.println(&quot;color of apple is &quot; + color);
+
+}
+}
