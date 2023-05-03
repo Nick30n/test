9162c930b8fb90589d1a5b493184e6414a2c9bfb
Nick30n <132458776+Nick30n@users.noreply.github.com>
2023-05-03 22:23:30 +0530

    Update orange.java

--- rbca21213/src/rbca21213/orange.java
+++ rbca21213/src/rbca21213/orange.java
@@ -11,6 +11,6 @@ public static void main(String[] args) {
 orange greenApple = new orange(&quot;green&quot;);
 String color = greenApple.getColor();
 System.out.println(&quot;color of apple is &quot; + color);
-
+//edhyd9qup98jd
 }
 }
